9e89ad53be156c49b2f9296bfcc650cd6e90aeea
Chance Leed <chanceleed@n3-2-194.dhcp.drexel.edu>
2020-02-22 22:02:17 -0500

    we got image upload and first attempt at sending images to endpoint

--- src/Components/InputForm/InputFormClass.js
+++ src/Components/InputForm/InputFormClass.js
@@ -1,4 +1,6 @@
 import React from "react";
+const axios = require("axios");
+
 
 export class InputForm extends React.Component {
 
@@ -12,11 +14,32 @@ export class InputForm extends React.Component {
             'Forehead Size': '',
             'Chin Size': '',
             'Lip Size': '',
-            'Eye Height': ''
+            'Eye Height': '',
+            file:null
         };
-
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    onFormSubmit(e){
+      e.preventDefault();
+      const formData = new FormData();
+      formData.append('myImage',this.state.file);
+      const config = {
+        headers: {
+          'content-type':'multipart/form-data'
+        }
+      };
+      axios.post("/upload",formData,config)
+        .then((response) => {
+          alert("The file is successfully uploaded");
+        }).catch((error) =>{
+      });
+    }
+    onChange(e){
+      this.setState({file:e.target.files[0]});
     }
 
     handleChange(event) {
@@ -31,62 +54,67 @@ export class InputForm extends React.Component {
 
     render() {
         return(
-            <div className="InputForm">
-                {this.renderTextForm()}
-                {this.renderImageInput()}
-            </div>
+          <div className="InputForm">
+            {this.renderTextForm()}
+            {this.renderImageInput()}
+
+          </div>
         );
     }
 
     renderTextForm() {
         return(
-            <div className="textForm">
-                <form className="testForm" onSubmit={this.handleSubmit}>
-                    <label>
-                        Eye Color
-                        <input className="testFormInput" name="eyeColor" type="text" onChange={this.handleChange} />
-                    </label>
-                    <label>
-                        Skin Color
-                        <input className="testFormInput" name="skinColor" type="text" onChange={this.handleChange} />
-                    </label>
-                    <label>
-                        Hair Color
-                        <input className="testFormInput" name="hairColor" type="text" onChange={this.handleChange} />
-                    </label>
-                    <label>
-                        Face Ratio
-                        <input className="testFormInput" name="faceRatio" type="text" onChange={this.handleChange} />
-                    </label>
-                    <label>
-                        Forehead Size
-                        <input className="testFormInput" name="foreheadSize" type="text" onChange={this.handleChange} />
-                    </label>
-                    <label>
-                        Chin Size
-                        <input className="testFormInput" name="chinSize" type="text" onChange={this.handleChange} />
-                    </label>
-                    <label>
-                        Lip Size
-                        <input className="testFormInput" name="lipSize" type="text" onChange={this.handleChange} />
-                    </label>
-                    <label>
-                        Eye Height
-                        <input className="testFormInput" name="eyeHeight" type="text" onChange={this.handleChange} />
-                    </label>
-                    <input className="submit" type="submit" value="Submit"/>
-                </form>
-            </div>
+        <div className="textForm">
+            <form className="testForm" onSubmit={this.handleSubmit}>
+                <label>
+                    Eye Color
+                    <input className="testFormInput" name="eyeColor" type="text" onChange={this.handleChange} />
+                </label>
+                <label>
+                    Skin Color
+                    <input className="testFormInput" name="skinColor" type="text" onChange={this.handleChange} />
+                </label>
+                <label>
+                    Hair Color
+                    <input className="testFormInput" name="hairColor" type="text" onChange={this.handleChange} />
+                </label>
+                <label>
+                    Face Ratio
+                    <input className="testFormInput" name="faceRatio" type="text" onChange={this.handleChange} />
+                </label>
+                <label>
+                    Forehead Size
+                    <input className="testFormInput" name="foreheadSize" type="text" onChange={this.handleChange} />
+                </label>
+                <label>
+                    Chin Size
+                    <input className="testFormInput" name="chinSize" type="text" onChange={this.handleChange} />
+                </label>
+                <label>
+                    Lip Size
+                    <input className="testFormInput" name="lipSize" type="text" onChange={this.handleChange} />
+                </label>
+                <label>
+                    Eye Height
+                    <input className="testFormInput" name="eyeHeight" type="text" onChange={this.handleChange} />
+                </label>
+                <input className="submit" type="submit" value="Submit"/>
+            </form>
+        </div>
         );
     }
 
     renderImageInput() {
-        return(
-            <div className="ImageInput">
-                <input type='file' />
-                <button>Upload</button>
-            </div>
-        );
+      return(
+        <div className="ImageInput">
+        <form onSubmit={this.onFormSubmit}>
+        <h1>File Upload</h1>
+        <input type='file' name='submitImage' onChange={this.onChange} />
+        <button type='submit'>Upload</button>
+        </form>
+
+        </div>
+      );
     }
 
 }
