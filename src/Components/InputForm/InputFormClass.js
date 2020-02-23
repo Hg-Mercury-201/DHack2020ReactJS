27deba53d67ac17a82841a8f9b9dbf0148377a71
Nathan Beebe <njb99@drexel.edu>
2020-02-22 21:23:12 -0500

    Code Splitting

---
+++ src/Components/InputForm/InputFormClass.js
@@ -0,0 +1,92 @@
+import React from "react";
+
+export class InputForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            'Eye Color': '',
+            'Skin Color': '',
+            'Hair Color': '',
+            'Face Ratio': '',
+            'Forehead Size': '',
+            'Chin Size': '',
+            'Lip Size': '',
+            'Eye Height': ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+        console.log('state ',this.state);
+    }
+
+    handleSubmit(event) {
+        alert('A form was submitted: ' + this.state);
+        event.preventDefault();
+    }
+
+    render() {
+        return(
+            <div className="InputForm">
+                {this.renderTextForm()}
+                {this.renderImageInput()}
+            </div>
+        );
+    }
+
+    renderTextForm() {
+        return(
+            <div className="textForm">
+                <form className="testForm" onSubmit={this.handleSubmit}>
+                    <label>
+                        Eye Color
+                        <input className="testFormInput" name="eyeColor" type="text" onChange={this.handleChange} />
+                    </label>
+                    <label>
+                        Skin Color
+                        <input className="testFormInput" name="skinColor" type="text" onChange={this.handleChange} />
+                    </label>
+                    <label>
+                        Hair Color
+                        <input className="testFormInput" name="hairColor" type="text" onChange={this.handleChange} />
+                    </label>
+                    <label>
+                        Face Ratio
+                        <input className="testFormInput" name="faceRatio" type="text" onChange={this.handleChange} />
+                    </label>
+                    <label>
+                        Forehead Size
+                        <input className="testFormInput" name="foreheadSize" type="text" onChange={this.handleChange} />
+                    </label>
+                    <label>
+                        Chin Size
+                        <input className="testFormInput" name="chinSize" type="text" onChange={this.handleChange} />
+                    </label>
+                    <label>
+                        Lip Size
+                        <input className="testFormInput" name="lipSize" type="text" onChange={this.handleChange} />
+                    </label>
+                    <label>
+                        Eye Height
+                        <input className="testFormInput" name="eyeHeight" type="text" onChange={this.handleChange} />
+                    </label>
+                    <input className="submit" type="submit" value="Submit"/>
+                </form>
+            </div>
+        );
+    }
+
+    renderImageInput() {
+        return(
+            <div className="ImageInput">
+                <input type='file' />
+                <button>Upload</button>
+            </div>
+        );
+    }
+
+}
